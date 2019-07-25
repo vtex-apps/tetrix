9bfc92c449f1dab6474732c6dfa2667fadfce9a2
victorhmp <victor2142@gmail.com>
2019-07-25 16:50:47 -0300

    Update latest news section

--- react/components/News.tsx
+++ react/components/News.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 
 const News = () => {
-
   const [open, setOpen] = useState(false)
 
   return (
@@ -10,27 +9,47 @@ const News = () => {
       <div>
         <div className="flex flex-column pv5">
           <article>
-            <p className="t-heading-4 ttu link no-underline">
+            <h4 className="t-heading-4 ttu link no-underline">
               <a href="/tetrix/examples" className="link no-underline">
                 CONFIRA EXEMPLOS DE QUESTÕES DO TETRIX!
               </a>
+            </h4>
+            <small className="t-small">24 de julho de 2019</small>
+          </article>
+          <article>
+            <h4
+              className="t-heading-4 ttu pointer"
+              onClick={() => setOpen(!open)}
+            >
+              TESTE DE COMUNICAÇÃO
+            </h4>
+            <p hidden={!open} className="w-80-l lh-copy">
+              Todos os inscritos no Desafio Tetrix receberam um e-mail teste
+              hoje (24/07). Caso não tenha recebido ainda, verifique sua caixa
+              de spam. Caso não encontre, ocorreu um erro técnico. Para garantir
+              que recebera o teste no sábado, nos envie um e-mail para{' '}
+              <a href={'mailto:ola@tetrixodesafio.com'}>
+                ola@tetrixodesafio.com
+              </a>{' '}
+              comunicando a falha técnica.
             </p>
             <small className="t-small">24 de julho de 2019</small>
           </article>
           <article>
-            <p className="t-heading-4 ttu pointer" onClick={() => setOpen(!open)}>
-              TESTE DE COMUNICAÇÃO
-            </p>
-            <p hidden={!open} className="w-80-l lh-copy">
-              Todos os inscritos no Desafio Tetrix receberam um e-mail teste hoje (24/07).
-              Caso não tenha recebido ainda, verifique sua caixa de spam. Caso não encontre, ocorreu um erro técnico.
-              Para garantir que recebera o teste no sábado, nos envie um e-mail para <a href={'mailto:ola@tetrixodesafio.com'}>ola@tetrixodesafio.com</a> comunicando a falha técnica.
-            </p>
-            <small className="t-small">24 de julho de 2019</small>
+            <h4 className="t-heading-4 ttu link no-underline">
+              <a
+                href="https://drive.google.com/file/d/13-H1gOh1NT1uBloyHnnTYSI6EytIlrgR/view?usp=sharing"
+                className="link no-underline ttu"
+              >
+                Instruções finais - Primeira fase Desafio Tetrix
+              </a>
+            </h4>
+            <small className="t-small">25 de julho de 2019</small>
           </article>
         </div>
       </div>
     </section>
-)}
+  )
+}
 
 export default News
